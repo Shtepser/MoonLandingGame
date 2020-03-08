04e4548065385746b5d104058b17909e55cc312c
Shtepser <shtepser@yandex.ru>
2020-03-08 15:19:47 +0300

    Добавлены тесты для корабля

--- tests/moonlanding_tests.py
+++ tests/moonlanding_tests.py
@@ -1,14 +1,5 @@
 from nose.tools import *
 import moonlanding
 
-
-def setup():
-    print("Установка...")
-
-
-def teardown():
-    print("Завершение...")
-
-
 def test_basic():
     print("Выполнение...")
